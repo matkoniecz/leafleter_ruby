801c76346998818eab5a50d0236e32af4abf589b
Mateusz Konieczny <matkoniecz@gmail.com>
2016-11-23 21:53:59 +0100

    switch to https for positron
    source: http://gis.stackexchange.com/questions/133845/cartodb-ssl-version-of-the-light-all-basemap

--- lib/leafleter.rb
+++ lib/leafleter.rb
@@ -6,7 +6,7 @@ class Leafleter
   end
 
   def self.get_positron_tile_Layer()
-    return "L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+    return "L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
         attribution: '#{openstreetmap_copyright_notice}, basemap: &copy; <a href=\"http://cartodb.com/attributions\">CartoDB</a>',
         subdomains: 'abcd',
         maxZoom: 19
