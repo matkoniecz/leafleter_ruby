2e0dc0951e264fa54493179fa1883aa4dc7ed448
Mateusz Konieczny <matkoniecz@gmail.com>
2022-06-01 19:32:31 +0200

    add sanity bash header

--- reinstall.sh
+++ reinstall.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+set -Eeuo pipefail
+IFS=$'\\n\\t'
+err_report() {
+    echo "Error on line $1"
+}
+trap 'err_report $LINENO' ERR
+
 gem build *.gemspec
 gem install --user-install *.gem 
 #gem push *.gem 
